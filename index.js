91282cf0a37a38ca40b44424b0ea136ade7477bb
Sergio Spina <sergio.spina@spindox.it>
2024-06-16 20:04:46 +0200

    ready for publishing

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ import clear from 'clear';
 import { output } from './src/output.js';
 
 const borderParams = {
-  textColor: 'white',
   // borderColor: 'green',
-  borderStyle: 'bold',
-  dimBorder: false,
+  borderStyle: 'round',
+  // single | double | round | bold | singleDouble | classic | arrow | none
+  dimBorder: true,
+  // title: '',
+  // titleAlignment: ''
+  // left | center | right
   padding: 1,
-  margin: 0,
+  margin: 1,
 };
 
 clear();
