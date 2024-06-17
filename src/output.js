14d9006b82682f79518fbb0496d04fec7a849148
Sergio Spina <sergio.spina@spindox.it>
2024-06-17 17:08:05 +0200

    rebuilding

--- src/output.js
+++ src/output.js
@@ -1,14 +1,15 @@
 import chalk from 'chalk';
 
-export const output = `       Sergio Spina / Thorn Duke
+export const output = `             Sergio Spina / Thorn Duke
 
-  Work:  FullStack Developer 🛩
+    Work:  FullStack Developer 🛩
 
-   npm:  ${chalk.yellow('https://www.npmjs.com/~thornduke')}
-GitHub:  ${chalk.yellow('https://github.com/ThornDuke')}
-  Blog:  ${chalk.yellow('https://www.samsahara.it/')}
- Email:  ${chalk.yellow('se.spina@hotmail.com')}
+     npm:  ${chalk.yellow('https://www.npmjs.com/~thornduke')}
+LinkedIn:  ${chalk.yellow('https://www.linkedin.com/in/sergio-spina/')}
+  GitHub:  ${chalk.yellow('https://github.com/ThornDuke')}
+    Blog:  ${chalk.yellow('https://www.samsahara.it/')}
+   Email:  ${chalk.yellow('se.spina@hotmail.com')}
 
-  Card:  ${chalk.yellow('npx thorn-duke')}
+    Card:  ${chalk.yellow('npx thorn-duke')}
 `;
 console.log(output);
