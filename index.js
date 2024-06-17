3bf3297ef26685295aa596ce8c9246e006992408
Sergio Spina <sergio.spina@spindox.it>
2024-06-17 12:09:52 +0200

    fully working

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-#!/usr/bin/env node
-
 import boxen from 'boxen';
 import clear from 'clear';
 import { output } from './src/output.js';
 
-const borderParams = {
-  borderStyle: 'round',
-  dimBorder: true,
-  padding: 1,
-  margin: 1,
-};
+export default function card() {
+  const borderParams = {
+    borderStyle: 'round',
+    dimBorder: true,
+    padding: 1,
+    margin: 1,
+  };
 
-clear();
-console.log(boxen(output, borderParams));
+  clear();
+  console.log(boxen(output, borderParams));
+}
